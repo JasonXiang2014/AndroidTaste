d129ff1eb6dd349ad4a5c3d38c1f4a76b39eb15c
JasonXiang2014 <931477935@qq.com>
2016-11-23 15:33:17 +0800

    Add Scroller

--- app/src/main/java/jasonxiang/com/doItYourself/xj/ui/main/Page3Fragment.java
+++ app/src/main/java/jasonxiang/com/doItYourself/xj/ui/main/Page3Fragment.java
@@ -1,15 +1,24 @@
 package jasonxiang.com.doItYourself.xj.ui.main;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import butterknife.BindView;
 import jasonxiang.com.doItYourself.R;
 import jasonxiang.com.doItYourself.xj.base.BaseFragment;
+import jasonxiang.com.doItYourself.xj.common.widget.ScrollerView;
 
 /**
  * Created by xiangjain on 2016/11/10.
  */
 
 public class Page3Fragment extends BaseFragment {
+
+    @BindView(R.id.scrollerView)
+    ScrollerView scrollerView;
+    @BindView(R.id.btnScroll)
+    Button btnScroll;
 
     @Override
     protected int getContentViewId() {
@@ -19,5 +28,12 @@ public class Page3Fragment extends BaseFragment {
     @Override
     protected void init(Bundle savedInstanceState) {
 
+        btnScroll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scrollerView.smoothScrollTo(-400, -400);
+            }
+        });
     }
+
 }
