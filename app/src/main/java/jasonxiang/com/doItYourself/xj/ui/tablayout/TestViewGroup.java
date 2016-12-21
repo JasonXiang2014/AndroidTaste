be1e098bf9662ddedac4caf4b841edf2d8d89bb7
JasonXiang2014 <xiang_jerry@sina.com>
2016-12-21 15:52:18 +0800

    Use offsetTopAndBottom 如果在viewGroup的dispatchTouEvent返回true，那么自身和子view都不会收到后续 事件

--- app/src/main/java/jasonxiang/com/doItYourself/xj/ui/tablayout/TestViewGroup.java
+++ app/src/main/java/jasonxiang/com/doItYourself/xj/ui/tablayout/TestViewGroup.java
@@ -5,10 +5,11 @@ import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 /**
  * Created by JasonXiang on 2016/12/21.
@@ -17,6 +18,16 @@ import android.widget.Toast;
 public class TestViewGroup extends LinearLayout {
 
     public static final String TAG = TestViewGroup.class.getSimpleName();
+
+    private int interceptX;
+    private int interceptY;
+
+    private int xLast;
+    private int yLast;
+
+    private int mPagingTouchSlop;
+    private float mResistance = 1.7f;
+
 
     public TestViewGroup(Context context) {
         super(context);
@@ -28,6 +39,8 @@ public class TestViewGroup extends LinearLayout {
 
     public TestViewGroup(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        final ViewConfiguration conf = ViewConfiguration.get(getContext());
+        mPagingTouchSlop = conf.getScaledTouchSlop() * 2;
     }
 
     @Override
@@ -36,7 +49,7 @@ public class TestViewGroup extends LinearLayout {
         super.onFinishInflate();
     }
 
-    public void setHeader(View header){
+    public void setHeader(View header) {
         ViewGroup.LayoutParams lp = header.getLayoutParams();
         if (lp == null) {
             lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
@@ -47,19 +60,66 @@ public class TestViewGroup extends LinearLayout {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        Toast.makeText(getContext(), "onMeasure", Toast.LENGTH_LONG).show();
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        Toast.makeText(getContext(), "onLayout", Toast.LENGTH_LONG).show();
         super.onLayout(changed, l, t, r, b);
     }
 
     @Override
     protected void dispatchDraw(Canvas canvas) {
-        Toast.makeText(getContext(), "dispatchDraw", Toast.LENGTH_LONG).show();
         super.dispatchDraw(canvas);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        boolean intercept = false;
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                interceptX = (int) ev.getX();
+                interceptY = (int) ev.getY();
+                xLast = (int) ev.getX();
+                yLast = (int) ev.getY();
+                intercept = false;
+                break;
+            case MotionEvent.ACTION_MOVE:
+
+                int deltaX = (int) (ev.getX() - interceptX);
+                int deltaY = (int) (ev.getY() - interceptY);
+                if (Math.abs(deltaY) > Math.abs(deltaX) && Math.abs(deltaY) > mPagingTouchSlop)
+                    intercept = true;
+                break;
+            case MotionEvent.ACTION_UP:
+                intercept = false;
+                break;
+            default:
+                break;
+        }
+        return intercept;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+
+                int deltaX = (int) (event.getX() - xLast);
+                int deltaY = (int) (event.getY() - yLast);
+
+                offsetTopAndBottom((int) (deltaY / mResistance));
+                offsetLeftAndRight((int) (deltaX / mResistance));
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                break;
+        }
+        xLast = (int) event.getX();
+        yLast = (int) event.getY();
+        return true;
     }
 }
